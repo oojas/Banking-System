51b1305b04748c7ec6d2ac1ad16d51430d2ec795
oojas <oojasgupta@gmail.com>
2022-03-17 23:12:31 +0530

    Rerfactored the code

---
+++ handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"net/http"
+)
+
+type Courses struct {
+	Name string
+	Link string
+}
+
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request) {
+	courses := []Courses{
+		{"flutter", "https://google.com"},
+		{"Hadoop", "https:udemy.com"},
+	}
+	w.Header().Add("Content-Type", "application/xml")
+	xml.NewEncoder(w).Encode(courses)
+}
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ojas Gupta", "Jammu", "180012"},
+		{"Ritu Gupta", "Jammu", "180013"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+
+}
