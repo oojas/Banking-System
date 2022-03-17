afa61761b60bf24f10b47e44b6f708dd20617d05
oojas <oojasgupta@gmail.com>
2022-03-17 20:55:20 +0530

    passing json encoding

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 )
+
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+}
 
 func main() {
 	// Setting up of routes.
 	http.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World!")
 	})
+	http.HandleFunc("/customers", getAllCustomers)
 	// starting the server.
 	// listen and serve function returns an error so should always be wrapped with log.fatal
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	http.ListenAndServe("localhost:8000", nil)
+}
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Ojas Gupta", "Jammu", "180012"},
+		{"Ritu Gupta", "Jammu", "180013"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
+
 }
